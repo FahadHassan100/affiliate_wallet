eaff32defdcdd4faede1fbe7204adcfb3633643b
Fahad Hassan <fahadhassan921@gmail.com>
2025-11-26 17:29:22 +0400

    update image 3

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,18 @@ export default function Home() {
   //const affiliateID = parseInt(searchParams.get("uid"));
   const affiliateID = 1111;
 
+  // Define the asset path based on where the app is deployed
+  const ASSET_PATH =
+    process.env.NODE_ENV === "production" &&
+    process.env.NEXT_PUBLIC_ASSET_PREFIX
+      ? process.env.NEXT_PUBLIC_ASSET_PREFIX
+      : "";
+  if (ASSET_PATH) {
+    console.log("This is Asset Path", ASSET_PATH);
+  } else {
+    console.log("Nothing");
+  }
+
   return (
     <div className="min-h-screen bg-gray-50">
       {checkAffT && <p>This is Type {checkAffT}</p>}
@@ -24,7 +36,13 @@ export default function Home() {
         <div className="mainContent w-full">
           <div className="row">
             <div className="headerSection flex justify-center pt-2">
-              <Image src={logoImage} alt="logo" width={300} />
+              <Image
+                src={`${ASSET_PATH}/logo.png`} // Use the dynamic path
+                alt="logo"
+                width={300}
+                height={100} // Always include height for optimization
+              />
+              {/* <Image src={logoImage} alt="logo" width={300} /> */}
             </div>
           </div>
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig:NextConfig = {
   /* config options here */
   assetPrefix: '/my-wallet',
+  env: {
+    NEXT_PUBLIC_ASSET_PREFIX: '/my-wallet',
+  },
   output: "standalone",
   experimental: {
     serverActions: {
